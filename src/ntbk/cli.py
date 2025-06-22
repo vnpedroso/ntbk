135d8f6fbc52eee4220791367f81bd726e240479
vnpedroso <npp.vinicius@gmail.com>
2025-06-22 20:58:22 -0300

    fixing new subcommand

--- src/ntbk/cli.py
+++ src/ntbk/cli.py
@@ -6,23 +6,23 @@ from ntbk.crud.pages import ensure_ntbk_page
 @click.group()
 @click.pass_context
 def ntbk(ctx):
+    """Your terminal based personal notebook application"""
     ensure_ntbk_home()
     ctx.ensure_object(dict)
 
 
 @ntbk.command()
 @click.pass_context
-@click.option("--page", is_flag=True, type=str , help="Create a new notebook page",)
-@click.option("--note", is_flag=True, type=str, help="Create a new note in the notebook page")
-@click.argument("name", required=True)
-def new(ctx, page, note, name):
-    if page and note:
-        click.secho("You must specify either a page or a note, not both.", fg="yellow", bold=True)
-        return
-    if not (page and note):
-        click.secho("You must specify either a page or a note.", fg="yellow", bold=True)
+@click.option("--page", type=str , help="Create a new notebook page",)
+@click.option("--note", type=str, help="Create a new note in the notebook page")
+def new(ctx, page, note):
+    """Create a new notebook page or note"""
+    
+    if not (page or note):
+        click.secho("You must specify either a page or a note.", fg="red", bold=True)
         return
     
     if page:
-        ensure_ntbk_page(name)
+        ensure_ntbk_page(page)
+
 
