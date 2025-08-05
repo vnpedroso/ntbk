2b73ac50b5e0f9e0eefc09492ed531540d88e0dd
vnpedroso <npp.vinicius@gmail.com>
2025-08-05 00:10:23 -0300

    creating tabulate function to display notes

--- src/ntbk/crud/home.py
+++ src/ntbk/crud/home.py
@@ -2,7 +2,7 @@
 from ntbk.utils.constants import NTBK_HOME
 
 def ensure_ntbk_home() -> None:
-    """Creates notebook base directory at users $HOME if it does not exist"""
+    """Creates notebook base directory at users $HOME, if it does not exist."""
 
     if not NTBK_HOME.exists():
         NTBK_HOME.mkdir(exist_ok=True)
--- src/ntbk/utils/utils.py
+++ src/ntbk/utils/utils.py
@@ -1,3 +1,5 @@
+from rich.console import Console
+from rich.table import Table   
 
 def treat_input(text: str) -> str:
     """
@@ -12,3 +14,36 @@ def treat_input(text: str) -> str:
     """
 
     return '_'.join(text.strip().split()).lower()
+
+
+def tabulate(title: str, headers: list[str], rows: list[list]) -> None:
+    """
+    Prints a cool table with the inputted data
+
+    Args:
+        title (str): the title of the table
+        headers (list[str]): a list containing the columns titles
+        rows (list[list]): a list with all the data points of the table, each a list on its own
+    """
+
+    table = Table(title=title)
+
+    for idx in range(1,len(headers)):
+        assert len(rows[idx]) == len(rows[idx - 1]), f"row {idx} and {idx - 1} have different lengths!"
+
+    assert len(headers) == len(rows[0]), f"column titles and rows are not matching"
+
+    for idx in range(0, len(headers)):
+        table.add_column(headers[idx], justify="right",no_wrap=True)
+
+    for row in rows:
+        table.add_row(*row)
+
+    console = Console()
+    console.print(table)
+
+
+    
+
+
+
