40590419e870397c58e53389b37a7f6d9ed551b8
vnpedroso <npp.vinicius@gmail.com>
2026-01-16 11:11:36 -0300

    adding bookmark logic to cli commands

--- src/ntbk/cli.py
+++ src/ntbk/cli.py
@@ -1,35 +1,51 @@
 import click
 
+from ntbk.utils.constants import NTBK_HOME
+
+from ntbk.crud.bookmark import ensure_bookmark, read_bookmark, write_bookmark
 from ntbk.crud.home import ensure_ntbk_home
 from ntbk.crud.pages import ensure_ntbk_page
 
 @click.group()
 @click.pass_context
-def ntbk(ctx):
+def ntbk(ctx: click.Context):
     """Your terminal based personal notebook application"""
     ensure_ntbk_home()
+    ensure_bookmark()
     ctx.ensure_object(dict)
+    ctx.obj["bookmark"] = read_bookmark()
 
 @ntbk.group
 @click.pass_context
-def new(ctx):
+def new(ctx: click.Context):
     """
-    ntbk new page --> creates a new PAGE\n
-    ntbk new note --> creates a new NOTE
+    ntbk new page:  creates a new page\n
+    ntbk new note:  creates a new note
     """
-    pass
+    return
 
 @new.command
 @click.pass_context
 @click.argument("page_name")
-def page(ctx, page_name):
-    """ntbk new page <page_name> --> creates new page with name <page_name>"""
+def page(ctx: click.Context, page_name):
+    """ntbk new page <dummy>:   creates a new page named <dummy>"""
+
     ensure_ntbk_page(page_name)
+    write_bookmark(page_name)
     click.secho(f'page "{page_name}" successfully created', fg="green", bold=True)
 
 @new.command
 @click.pass_context
-def note(ctx):
-    pass
+@click.option("--page", help="name of the page on which to the note")
+def note(ctx: click.Context, page):
 
-
+    bmk = ctx.obj["bookmark"]
+    
+    if not(page and bmk):
+        click.secho("must create at least one page before creating notes!\n", bold=True, fg="red")
+        raise click.UsageError("No page found")
+    
+    if not page:
+        return
+    
+    
