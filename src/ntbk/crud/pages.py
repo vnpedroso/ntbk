a4643f636996f5502fd39467c7115494f095751a
vnpedroso <npp.vinicius@gmail.com>
2026-01-18 10:39:27 -0300

    creating funciton to read notebook pages

--- src/ntbk/crud/pages.py
+++ src/ntbk/crud/pages.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
-from ntbk.utils.constants import NTBK_HOME
-from ntbk.utils.utils import treat_input, build_ntbk_path
+from ntbk.utils.constants import NTBK_HOME, SEPARATOR
+from ntbk.utils.utils import treat_input, build_ntbk_path, tabulate
 
 def ensure_ntbk_page(pg_name: str) -> None:
     """creates notebook page if it does not exist
@@ -26,5 +26,28 @@ def is_page_blank(fpage: Path) -> bool:
     """
     with open(fpage, mode="r") as f:
         first_char = f.read(3)
-        
+
     return False if first_char else True
+
+
+def read_page(fpage: Path, page_name: str) -> None:
+    """displays all notes in a page
+
+    Args:
+        fpage (Path): the full path to the page
+        page_name (str): the page name
+    """
+
+    headers = ["id","created_at","content"]
+    rows = []
+
+    with open(fpage, mode="r") as f:
+        for line in f:
+            row = line.strip("\n").split(SEPARATOR)
+            rows.append(row)
+
+    tabulate(
+        title=page_name,
+        headers=headers,
+        rows=rows
+    )
