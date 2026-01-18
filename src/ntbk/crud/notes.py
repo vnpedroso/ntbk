00d2cc3edd870cdaa56bef56ce7d90ef39289cd7
vnpedroso <npp.vinicius@gmail.com>
2026-01-17 23:04:02 -0300

    func to check chart limit + func to write note

--- src/ntbk/crud/notes.py
+++ src/ntbk/crud/notes.py
@@ -1,11 +1,36 @@
+from pathlib import Path
 
 
-def write_note(pg_name: str) -> None:
-    """Writes a new note to the specified notebook page.
+def is_within_char_limit(note: str) -> bool:
+    """checks if note is complying to 144 char limit
 
     Args:
-        pg_name (str): The name of the notebook page to write the note to.
+        note (str): note to be written
+
+    Returns:
+        bool: True if note is within 144 char limit, else False
     """
 
-    pass
+    return False if len(note) > 144 else True
+
+
+def write_note(note: str, append: bool, fpage: Path) -> None:
+    """writes note in its respective page
+
+    Args:
+        note (str): the note to be written
+        append (bool): if the note is to be appended or written
+        fpage (Path): full page to the note's page
+    """
+
+    if append:
+        with open(fpage, mode="a") as f:
+            f.write(note)
+            return
+    
+    with open(fpage, mode="w") as f:
+        f.write("\n" + note)
+        return
+
+
 
